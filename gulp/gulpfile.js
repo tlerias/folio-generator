8d0b2b861d1b306afd38071e6d6379dab0d79bf6
Tara Lerias <tlerias@gmail.com>
2016-09-14 16:47:05 -0400

    commit initial setup and files for folio

---
+++ gulp/gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+ 
+gulp.task('default', () => {
+  return gulp.src('src/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+        presets: ['es2015']
+    }))
+    .pipe(concat('folio-generator.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'));
+});
+
+// TODO: add gulp clean, build and deploy
---
+++ src/folio.js
@@ -0,0 +1,40 @@
+( function () {
+
+	/* 
+
+	Folio Constructor
+	@params
+	@elementContainer: the element that this will replace
+	@resumeSet: array of jobs
+	@options options for the Folio
+
+
+	*/
+	class Folio {
+
+		constructor (elementContainer, resumeSet = [], options = {}) {
+
+		// Validation of element, the only required argument
+    	if (!elementContainer || (elementContainer.nodeName !== 'DIV' && elementContainer.tagName !== 'DIV')) return;
+
+    	this.skillExperience = _getSkillExperience(resumeSet);
+		}
+
+		getDateDifference: _getDateDifference
+	}
+
+	function _getSkillExperience (resumeSet) {
+
+	}
+
+	function _getDateDifference (dates, measurement) {
+
+	}
+
+	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+	  module.exports = Folio;
+	} else {
+	  window.Folio = Folio;
+	}
+
+})();
